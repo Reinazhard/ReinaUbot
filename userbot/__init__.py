7a97f613247669f698ae2d1387f1c19691a66fac
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-03-26 00:18:41 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -60,7 +60,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", None)
 BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", None))
 
 # Userbot logging feature switch.
-BOTLOG = sb(os.environ.get("BOTLOG", "True"))
+BOTLOG = sb(os.environ.get("BOTLOG", "False"))
 LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
 
 # Bleep Blop, this is a bot ;)
