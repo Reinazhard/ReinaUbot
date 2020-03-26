fff4f598c0a17e98169ffa9c3943e2af9c9fb6f9
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-03-27 00:27:40 +0700

    Add files via upload

---
+++ uranus_bot/providers/custom_recovery/twrp/twrp.py
@@ -0,0 +1,45 @@
+"""twrp custom recovery downloads scraper"""
+
+from bs4 import BeautifulSoup
+from aiohttp import ClientSession
+
+from uranus_bot.providers.utils.utils import fetch
+
+
+async def load_twrp_data():
+    """
+    load twrp for Xiaomi devices info
+    """
+    async with ClientSession() as session:
+        html = await fetch(session, 'https://twrp.me/Devices/Xiaomi/')
+        page = BeautifulSoup(html, 'html.parser')
+        devices = page.find("ul", {"class": "post-list"}).findAll('a')
+        data = {}
+        for i in devices:
+            info = {}
+            codename = i.text.split('(')[-1].split(')')[0]
+            if '/' in codename:
+                codename = codename.split('/')[0]
+            link = f'https://dl.twrp.me/{codename}/'
+            device = i.text
+            info.update({'name': device})
+            info.update({'link': link})
+            data.update({codename: info})
+        return data
+
+
+async def get_twrp(device, twrp_data):
+    """
+    fetch latest twrp links for a device
+    """
+    name = twrp_data[device]['name']
+    link = twrp_data[device]['link']
+    async with ClientSession() as session:
+        html = await fetch(session, link)
+        page = BeautifulSoup(html, 'html.parser').find('table').find('tr')
+        download = page.find('a')
+        dl_link = f"https://dl.twrp.me{download['href']}"
+        dl_file = download.text
+        size = page.find("span", {"class": "filesize"}).text
+        date = page.find("em").text.strip()
+        return {'name': name, 'dl_file': dl_file, 'dl_link': dl_link, 'size': size, 'date': date}
