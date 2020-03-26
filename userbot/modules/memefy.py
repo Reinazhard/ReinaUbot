81639953c3fac21a02ca005da68e3891083b0994
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-03-26 17:45:55 +0700

    Update memefy.py

--- userbot/modules/memefy.py
+++ userbot/modules/memefy.py
@@ -8,7 +8,7 @@ import os
 import requests
 import asyncio
 
-from userbot.modules.misc upload_download import humanbytes
+from userbot.modules.upload_download import humanbytes
 from userbot import TEMP_DOWNLOAD_DIRECTORY, CMD_HELP, bot
 from userbot.events import register
 
