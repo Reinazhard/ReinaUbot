3a67524a9d23ac14d3cd6f8d43353398d435938f
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-04 09:45:06 +0700

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -209,7 +209,7 @@ async def _(event):
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1111224224))
               await conv.send_message(f'/{phone}')
               response = await response
-              time.sleep(5)
+              time.sleep(3)
           except YouBlockedUserError: 
               await event.reply("```Unblock @ofoxr_bot plox```")
               return
@@ -226,7 +226,7 @@ async def _(event):
             response = conv.wait_event(events.NewMessage(incoming=True,from_users=1111224224))            
             await conv.send_message('/list')
             response = await response
-            asyncio.sleep(3)
+            time.sleep(3)
         except YouBlockedUserError:
             await event.reply("```Unblock @ofoxr_bot plox```")
             return
