dc5f07797e269969348f42be7ba72ab752c89171
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-04 10:38:25 +0700

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -214,7 +214,7 @@ async def _(event):
               await event.reply("```Unblock @ofoxr_bot plox```")
               return
           else:   
-             await event.edit(event.chat_id, response.message)
+             await event.edit(f"{response.message}")
 
 @register(outgoing=True, pattern="^.ofoxlist(?: |$)(.*)")
 async def _(event):
