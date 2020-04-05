a7f4a7e372a8c3a68c18dcbd47f48dd9c90c60c4
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-05 18:24:17 +0700

    Update android.py

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -212,8 +212,7 @@ async def _(event):
           except YouBlockedUserError: 
               await event.reply("```Unblock @ofoxr_bot plox```")
               return
-          elif:
-             time.sleep(3) 
+          elif time.sleep(3) 
              await event.edit("```Device not found\nUse .ofoxlist or .ofox list to get supported device list```")
           else:   
              await event.delete()   
