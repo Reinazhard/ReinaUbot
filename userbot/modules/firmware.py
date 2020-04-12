f3dcab4dbc1cdbaefce7dd7a787ca313df80b5b5
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-13 05:03:50 +0700

    Update firmware.py

--- userbot/modules/firmware.py
+++ userbot/modules/firmware.py
@@ -7,27 +7,27 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import bot, CMD_HELP
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.firmware(?: |$)(.*)")
+@register(outgoing=True, pattern="^.ofox(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    phone = event.pattern_match.group(1)
-    firmware = f"firmware"
+    link = event.pattern_match.group(1)
     chat = "@XiaomiGeeksBot"
+    firmware = f"firmware"
     await event.edit("```Processing```")
     async with bot.conversation("@XiaomiGeeksBot") as conv:
           try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=774181428))
-              await bot.send_message(f"/{firmware} {phone}")
+              await conv.send_message(f'/{firmware} {link}')
+              await bot.send_message(chat, link)
               response = await response
           except YouBlockedUserError:
-              await event.reply("```Unblock @XiaomiGeeksBot plox```")
+              await event.reply("```Unblock @ofoxr_bot plox```")
               return
-          if response.text.startswith("**Could not find f'{phone}' requested information!**"):
-             await event.edit("```Devices not found```")
           else:
              await event.delete()
              await bot.forward_messages(event.chat_id, response.message)
+
+
 
 CMD_HELP.update({
 "Firmware":
