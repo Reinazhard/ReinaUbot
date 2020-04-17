bd6f2cbd5e6c176047ccaa4876e55859eb747ab0
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-17 14:14:49 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -241,7 +241,7 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"`HaHa Yes!`\n`Your sticker has been kanged by` @eve_enryu!\
+        await args.edit(f"`HaHa Yes!`\n`Your sticker has been kanged by` f'{user.username}'!\
             \nPack can be found [here](t.me/addstickers/{packname})",
                         parse_mode='md')
 
