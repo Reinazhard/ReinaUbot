1439596dc71dbe703f8f902a439e45a3649a1ba2
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-22 22:32:50 +0700

    Update hacchi.py

--- userbot/modules/hacchi.py
+++ userbot/modules/hacchi.py
@@ -16,7 +16,7 @@ async def _(event):
         await event.edit(input_str)
         animation_chars = ["`Connecting To Hacked Private Server...`",
             "`Target Selected.`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            '"`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
             "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
@@ -26,8 +26,19 @@ async def _(event):
             "`Hacking... 100%\n█████████HACKED███████████ `",
             "`Targeted Account Hacked...\n\nPay 6969$ To this Nigga @eve_enryu To Remove This Hack`"]
     for i in animation_ttl:
+        animation_chars = ["`Connecting To Hacked Private Server...`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+            "`Hacking... 100%\n█████████HACKED███████████ `",
+            "`Targeted Account Hacked...\n\nPay 6969$ To this Nigga @eve_enryu To Remove This Hack`"]
         await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars)
+        await event.edit(animation_chars[i % 11])
             
             
 CMD_HELP.update({
