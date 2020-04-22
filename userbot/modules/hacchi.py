b892fe04266798a9ceb0d0d4f5d028a289ed8225
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-22 22:35:22 +0700

    Update hacchi.py

--- userbot/modules/hacchi.py
+++ userbot/modules/hacchi.py
@@ -14,19 +14,9 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     if input_str == "hacc":
         await event.edit(input_str)
-        animation_chars = ["`Connecting To Hacked Private Server...`",
-            "`Target Selected.`",
-            '"`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 6969$ To this Nigga @eve_enryu To Remove This Hack`"]
     for i in animation_ttl:
-        animation_chars = ["`Connecting To Hacked Private Server...`",
+        animation_chars = [
+            "`Connecting To Hacked Private Server...`",
             "`Target Selected.`",
             "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
