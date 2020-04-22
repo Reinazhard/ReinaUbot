f8dfce294688bb2018ca0d3ab6ea50f48ba71268
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-22 23:45:33 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -2,9 +2,6 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
-""" Userbot help command """
-import telegram
 
 from userbot import CMD_HELP, TG_BOT_USER_NAME_BF_HER, bot
 from userbot.events import register
