d8a76f601d4d4c23c3063efa4ea160c012c370f3
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-22 23:56:45 +0700

    Add files via upload

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -2,46 +2,27 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
 
-from userbot import CMD_HELP, TG_BOT_USER_NAME_BF_HER
+from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(pattern="^.help ?(.*)")
-async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = TG_BOT_USER_NAME_BF_HER
-        input_str = event.pattern_match.group(1)
-        if tgbotusername is None or input_str == "text":
-            string = ""
-            for i in CMD_HELP:
-                string += "ℹ️ " + i + "\n"
-                for iter_list in CMD_HELP[i]:
-                    string += "    `" + str(iter_list) + "`"
-                    string += "\n"
-                string += "\n"
-            if len(string) > 4095:
-                await bot.send_message(event.chat_id, "Do .help cmd")
-                await asyncio.sleep(5)
-            else:
-                await event.edit(string)
-        elif input_str:
-            if input_str in CMD_HELP:
-                string = "Commands found in {}:\n".format(input_str)
-                for i in CMD_HELP[input_str]:
-                    string += "    " + i
-                    string += "\n"
-                await event.edit(string)
-            else:
-                await event.edit(input_str + " is not a valid plugin!")
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
         else:
-            help_string = """Userbot Helper.. Provided by @A_Dark_Princ3 \n[Check out this dope af website](https://www.moddingunited.xyz/) \n
-`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
-            bot = tgbotusername
-            results = await bot.inline_query(tgbotusername, help_string)
-            await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
-            await event.delete()
+            await event.edit("Please specify a valid module name.")
+    else:
+        await event.edit("Please specify which module do you want help for !!\
+            \nUsage: .help <module name>")
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\n"
+        await event.reply(string)
