869ed7ce9b26b21b4ad8148e5b0e3d74ecc87274
Reina Hazard <60975832+Reinazhard@users.noreply.github.com>
2020-04-24 01:01:07 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -102,7 +102,7 @@ async def kang(args):
                 emoji = splat[1]
 
         packname = f"a{user.id}_by_{user.username}_{pack}"
-        packnick = f"Reina Nigga's kang pack Vol.{pack}"
+        packnick = f"{user.first_name} Nigga's kang pack Vol.{pack}"
         cmd = '/newpack'
         file = io.BytesIO()
 
